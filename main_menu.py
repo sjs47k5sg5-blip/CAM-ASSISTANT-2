cad02bf57ded61687fad1789ec06a3cf16fc7372
sjs47k5sg5-blip <sjs47k5sg5@privaterelay.appleid.com>
2026-07-02 06:16:28 +0300

    Delete main_menu.py

--- main_menu.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-main_menu=ReplyKeyboardMarkup(
-keyboard=[
-[KeyboardButton(text="📐 Режимы резания"),KeyboardButton(text="📊 Калькуляторы")],
-[KeyboardButton(text="⚙️ Генерация G-кода")],
-[KeyboardButton(text="🛠 Инструменты"),KeyboardButton(text="📚 G/M-коды")],
-[KeyboardButton(text="ℹ️ О программе")]
-],resize_keyboard=True)
