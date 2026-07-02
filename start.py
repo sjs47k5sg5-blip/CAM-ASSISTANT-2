1810f16e53352e965ee549613fafbe64a727d581
sjs47k5sg5-blip <sjs47k5sg5@privaterelay.appleid.com>
2026-07-02 05:35:12 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from keyboards.main_menu import main_menu
+from database import register_user
+router=Router()
+@router.message(CommandStart())
+async def start(message: Message):
+    register_user(message.from_user.id,message.from_user.username,message.from_user.first_name)
+    await message.answer("👋 Добро пожаловать в CNC Assistant Pro!", reply_markup=main_menu)
