85d70fcc8bcdb2bce2d56342c16808cbc1bb845e
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-05 11:27:09 +0300

    g

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import register_handlers
 
-TOKEN = "PUT_TOKEN_HERE"
+TOKEN = 8914558691:AAH79JQcoHLr7qEN8uC49cKqD3LYPbNrxXQ
 
 async def main():
     bot = Bot(token=TOKEN)
