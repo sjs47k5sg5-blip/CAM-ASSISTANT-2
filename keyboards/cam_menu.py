03aff3911ae55148e59aaef7bc1dd5b9ea594d6f
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-05 20:51:30 +0300

    в

--- keyboards/cam_menu.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-cam_menu = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="📐 Контур"), KeyboardButton(text="🟦 Карман")],
-        [KeyboardButton(text="📚 Справочник"), KeyboardButton(text="⚙️ Утилиты")],
-    ],
-    resize_keyboard=True
-)
