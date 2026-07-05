de1c6a97a649b765e8efe9d7d8fd066332d929fe
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-05 11:15:05 +0300

    h

---
+++ keyboards/cam_menu.py
@@ -0,0 +1,10 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+cam_menu = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text="📐 Контур", callback_data="cam_contour")
+    ],
+    [
+        InlineKeyboardButton(text="⬜ Карман", callback_data="cam_pocket")
+    ]
+])
