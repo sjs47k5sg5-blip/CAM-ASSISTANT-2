2129397a652379188cdb4ab4c4a631c46f8e0128
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-05 10:47:56 +0300

    н

--- keyboards/tools.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-tools_keyboard = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Твердосплавная")],
-        [KeyboardButton(text="HSS")]
-    ],
-    resize_keyboard=True
-)
--- services/cutting.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-def spindle_speed(vc: float, diameter: float) -> int:
-    return round((1000 * vc) / (math.pi * diameter))
-
-
-def feed_rate(rpm: int, teeth: int, fz: float) -> int:
-    return round(rpm * teeth * fz)
