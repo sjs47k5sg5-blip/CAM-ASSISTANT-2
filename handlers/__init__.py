1167fae10207d49138aaa3cfd7a2d1322d3366c0
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-05 09:35:48 +0300

    к

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,29 +1,33 @@
 from aiogram import Dispatcher
 
-from .back import router as back_router
-from .start import router as start_router
-
-from .face import router as face_router
-
+from .cam_router import router as cam_router
 from .drilling import router as drilling_router
+from .face import router as face_router
+from .milling import router as milling_router
 from .threading import router as threading_router
-from .milling_router import router as milling_router
-
-from .project import router as project_router
-from .reference import router as reference_router
+from .menu import router as menu_router
+from .start import router as start_router
+from .back import router as back_router
 
 
 def register_handlers(dp: Dispatcher):
-    dp.include_router(back_router)
-    dp.include_router(start_router)
 
-    dp.include_router(project_router)
+    # =========================
+    # CAM CORE ROUTER (MAIN)
+    # =========================
+    dp.include_router(cam_router)
 
-    dp.include_router(milling_router)
-    dp.include_router(face_router)
-    dp.include_router(contour_router)
-
+    # =========================
+    # MODULE ROUTERS
+    # =========================
     dp.include_router(drilling_router)
+    dp.include_router(face_router)
+    dp.include_router(milling_router)
     dp.include_router(threading_router)
 
-    dp.include_router(reference_router)
+    # =========================
+    # UI ROUTERS
+    # =========================
+    dp.include_router(menu_router)
+    dp.include_router(start_router)
+    dp.include_router(back_router)
