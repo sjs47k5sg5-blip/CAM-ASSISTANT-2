d291e878690d20af7d6fce886bb836d3f6b3c5fe
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-05 11:48:04 +0300

    н

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,6 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main_menu = ReplyKeyboardMarkup(
-    keyboard=[[KeyboardButton(text="CAM")]],
+    keyboard=[
+        [KeyboardButton(text="📐 CAM")],
+        [KeyboardButton(text="📚 Справочник")]
+    ],
     resize_keyboard=True
 )
