e4b73e8dffbed298a454b9633eb97a90c7da6eba
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-06 05:55:42 +0300

    л

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 
 from .menu import router as menu_router
-from .projects import router as projects_router
+from .cam_flow import router as cam_router
 from .milling import router as milling_router
 from .holes import router as holes_router
 from .manual import router as manual_router
@@ -9,8 +9,8 @@ from .about import router as about_router
 
 router = Router()
 
-router.include_router(menu_router)
-router.include_router(projects_router)
+router.include_router(menu_router)   # 🔥 ВАЖНО
+router.include_router(cam_router)
 router.include_router(milling_router)
 router.include_router(holes_router)
 router.include_router(manual_router)
