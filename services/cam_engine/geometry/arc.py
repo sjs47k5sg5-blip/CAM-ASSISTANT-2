93e0634549c778cfb966ccc684f3b02650789b5e
sjs47k5sg5-blip <298001490+sjs47k5sg5-blip@users.noreply.github.com>
2026-07-09 04:57:23 +0300

    f

--- services/cam_engine/geometry/arc.py
+++ services/cam_engine/geometry/arc.py
@@ -8,49 +8,91 @@ from .point import Point
 from .bbox import BoundingBox
 
 
-@dataclass(slots=True,frozen=True)
+@dataclass(slots=True, frozen=True)
 class Arc(Entity):
-    start: Point
-    end: Point
+    _start: Point
+    _end: Point
     center: Point
-    clockwise: bool=False
+    clockwise: bool = False
 
     @property
-    def radius(self)->float:
-        return self.center.distance_to(self.start)
+    def start(self) -> Point:
+        return self._start
 
     @property
-    def start_angle(self)->float:
-        return atan2(self.start.y-self.center.y,self.start.x-self.center.x)
+    def end(self) -> Point:
+        return self._end
 
     @property
-    def end_angle(self)->float:
-        return atan2(self.end.y-self.center.y,self.end.x-self.center.x)
+    def radius(self) -> float:
+        return self.center.distance_to(self._start)
 
-    def copy(self)->"Arc":
-        return Arc(self.start.copy(),self.end.copy(),self.center.copy(),self.clockwise)
+    @property
+    def start_angle(self) -> float:
+        return atan2(
+            self._start.y - self.center.y,
+            self._start.x - self.center.x,
+        )
 
-    def length(self)->float:
-        a1=self.start_angle
-        a2=self.end_angle
+    @property
+    def end_angle(self) -> float:
+        return atan2(
+            self._end.y - self.center.y,
+            self._end.x - self.center.x,
+        )
+
+    def copy(self) -> "Arc":
+        return Arc(
+            self._start.copy(),
+            self._end.copy(),
+            self.center.copy(),
+            self.clockwise,
+        )
+
+    def length(self) -> float:
+        a1 = self.start_angle
+        a2 = self.end_angle
+
         if self.clockwise:
-            if a1<a2:
-                a1+=2*pi
-            sweep=a1-a2
+            if a1 < a2:
+                a1 += 2 * pi
+            sweep = a1 - a2
         else:
-            if a2<a1:
-                a2+=2*pi
-            sweep=a2-a1
-        return self.radius*sweep
+            if a2 < a1:
+                a2 += 2 * pi
+            sweep = a2 - a1
 
-    def bbox(self)->BoundingBox:
-        return BoundingBox.from_points(self.start,self.end,self.center)
+        return self.radius * sweep
 
-    def translate(self,dx:float,dy:float)->"Arc":
-        return Arc(self.start.translate(dx,dy),self.end.translate(dx,dy),self.center.translate(dx,dy),self.clockwise)
+    def bbox(self) -> BoundingBox:
+        return BoundingBox.from_points(
+            self._start,
+            self._end,
+            self.center,
+        )
 
-    def rotate(self,angle_deg:float,origin:Point|None=None)->"Arc":
-        return Arc(self.start.rotate(angle_deg,origin),self.end.rotate(angle_deg,origin),self.center.rotate(angle_deg,origin),self.clockwise)
+    def translate(self, dx: float, dy: float) -> "Arc":
+        return Arc(
+            self._start.translate(dx, dy),
+            self._end.translate(dx, dy),
+            self.center.translate(dx, dy),
+            self.clockwise,
+        )
 
-    def point_at_angle(self,angle:float)->Point:
-        return Point(self.center.x+self.radius*cos(angle),self.center.y+self.radius*sin(angle))
+    def rotate(
+        self,
+        angle_deg: float,
+        origin: Point | None = None,
+    ) -> "Arc":
+        return Arc(
+            self._start.rotate(angle_deg, origin),
+            self._end.rotate(angle_deg, origin),
+            self.center.rotate(angle_deg, origin),
+            self.clockwise,
+        )
+
+    def point_at_angle(self, angle: float) -> Point:
+        return Point(
+            self.center.x + self.radius * cos(angle),
+            self.center.y + self.radius * sin(angle),
+        )
